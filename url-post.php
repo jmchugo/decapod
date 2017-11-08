b2de1a0372e65de0c2e7b057980e72207830cddf
jmchugo <jmchugo20@dtechhs.org>
2017-11-07 20:33:45 -0800

    Update url-post.php

--- url-post.php
+++ url-post.php
@@ -1,3 +1,11 @@
+nano / etc / nginx / sites-enabled / default
+location / {
+root / home / users / www /;
+index index.html index.htm;
+# Simply add this:
+error_page uri = $ 405;
+}
+
 <?php
 			if(isset($_POST["submit"])){
 			// Checking For Blank Fields..
